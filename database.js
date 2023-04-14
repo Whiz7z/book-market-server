e8356297cbd03b10e3b8f1d8e7ea8e2c1aef5dbe
Whiz7z <gjevstratov@gmail.com>
2023-04-14 18:34:24 +0100

    initialize server 4

--- database.js
+++ database.js
@@ -4,12 +4,12 @@ const connectToDatabase = async () => {
   try {
     mongoose.set("strictQuery", false);
     const connect = await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@place-project.98ixmv4.mongodb.net/?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@place-project.98ixmv4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     );
 
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error of mongo: ${error.message}`);
   }
 };
 

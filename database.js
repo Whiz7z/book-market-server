99a26fc634d67253cbdbe5a38dbc246113589c1e
Whiz7z <gjevstratov@gmail.com>
2023-04-14 17:59:42 +0100

    initialize server 2

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const connectToDatabase = async () => {
   try {
     mongoose.set("strictQuery", false);
     const connect = await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@place-project.98ixmv4.mongodb.net/flea-market?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@place-project.98ixmv4.mongodb.net/?retryWrites=true&w=majority`
     );
 
     console.log(`MongoDB Connected: ${connect.connection.host}`);
